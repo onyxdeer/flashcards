28f0212a95e09fe3a9b7e0bc231cd555914536bf
Patrick Xie <Patrick.y.xie@gmail.com>
2017-03-31 11:55:14 -0700

    set up test

--- server/tests/routesTest.js
+++ server/tests/routesTest.js
@@ -1,4 +1,14 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server/app');
+const should = chai.should();
 
+chai.use(chaiHttp)
 
-
+describe('Blobs', function() {
+  it('should list ALL blobs on /blobs GET');
+  it('should list a SINGLE blob on /blob/<id> GET');
+  it('should add a SINGLE blob on /blobs POST');
+  it('should update a SINGLE blob on /blob/<id> PUT');
+  it('should delete a SINGLE blob on /blob/<id> DELETE');
+});
