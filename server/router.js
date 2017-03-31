4b90b23da1925255f89a0edc1abf2dbf78d2be75
Patrick Xie <Patrick.y.xie@gmail.com>
2017-03-31 12:35:19 -0700

    made routes work

--- server/router.js
+++ server/router.js
@@ -1,13 +1,19 @@
-const { app } = require('server')
+// const { app } = require('server')
+const util = require('./util/util.js')
 
-console.log('our app is : ', app)
+const bindRoutes = (app) => {
+  app.route('/api/noris')
+    .post(util.tasks.noris.post)
+    .get(util.tasks.noris.get)
 
+  app.route('/api/bentos')
+    .post(util.tasks.bentos.post)
+    .get(util.tasks.bentos.get)
 
+app.route('/hello')
+  .get(function(req, res){
+    res.send('okayy')
+  })
+}
 
-app.route('/api/noris')
-  .post(util.tasks.noris.post)
-  .get(util.tasks.noris.get)
-
-app.route('/api/bentos')
-  .post(util.tasks.bentos.post)
-  .get(util.tasks.bentos.get)
+module.exports = bindRoutes
--- server/server.js
+++ server/server.js
@@ -3,23 +3,31 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const axios = require('axios');
 const db = require('../db/startdb.js');
+const router = require('./router.js')
 
 const util = require('./util/util.js')
+
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-exports.app = app 
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+router(app)
+// app.route('/hello')
+//   .get(function(req, res){
+//     res.send('okayy')
+//   })
 
 
 app.listen(PORT, function() {
   console.log('Obento express server connection established at:', PORT);
 });
 
+// exports.app = app 
 
 // app.route('/api/noris')
 //   .post(util.tasks.noris.post)
