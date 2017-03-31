79874cd1902a9e9cc9ea71e00b6739b780f57139
David Zou <david.zou@outlook.com>
2017-03-30 18:15:55 -0700

    Continuing to make changes to router and Display.

--- client/components/Display/Display.jsx
+++ client/components/Display/Display.jsx
@@ -26,11 +26,15 @@ class Display extends Component {
         }]
       },
       noriToDisplay: null,
-      currentNori: 0
+      currentNori: 0,
+      input: 0
     }
 
     this.prevNori = this.prevNori.bind(this);
     this.nextNori = this.nextNori.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.setNori = this.setNori.bind(this);
+    this.shuffleNori = this.shuffleNori.bind(this);
   }
 
   nextNori() {
@@ -55,10 +59,39 @@ class Display extends Component {
     });
   }
 
+  handleInput(event) {
+    this.setState({
+      input: event.target.value
+    }, console.log('handleInput: value for input has been set to', this.state.input));
+  }
+
+  setNori() {
+    event.preventDefault();
+    if (this.state.input > 0 && this.state.input < this.state.mockData.obento.length - 1) {
+      this.setState({
+        currentNori: this.state.input
+      });
+    } else {
+      alert('Invalid nori number, please enter another number.');
+    }
+    this.setState({  
+      noriToDisplay: this.state.mockData.obento[this.state.currentNori]
+    });
+    console.log('setNori: value for currentNori has been set to', this.state.currentNori);
+  }
+
+  shuffleNori() {
+
+  }
+
+  componentWillMount() {
+    // send an DB GET request for the flash cards here
+  }
+
   render() {
     return (
       <div>
-        <h1>Obento: {this.state.mockData.title}</h1>
+        <h1>Bento: {this.state.mockData.title}</h1>
           <FlipCard>
             <div>{this.state.noriToDisplay ? this.state.noriToDisplay.front : this.state.mockData.obento[0].front}</div>
             <div>{this.state.noriToDisplay ? this.state.noriToDisplay.back : this.state.mockData.obento[0].back}</div>
@@ -66,7 +99,12 @@ class Display extends Component {
           <div className='buttonSection'>
             <button className='btn btn-default' onClick={this.prevNori}>Previous Nori</button>
             <button className='btn btn-default' onClick={this.nextNori}>Next Nori</button>
+            <button className='btn btn-default' onClick={this.shuffleNori}>Shuffle Nori</button>
           </div>
+          <form className='changeToNoriSection' onSubmit={this.setNori}>
+            <label>Enter from 0 to {this.state.mockData.obento.length - 1} to go to that Nori: </label>
+            <input type='text' value={this.state.input} onChange={this.handleInput} placeholder='Enter a number here!' />
+          </form>
       </div>
     )
   }
