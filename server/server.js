8cfce3da3d9352fb8483767f5dc8112196d0c44e
Patrick Xie <Patrick.y.xie@gmail.com>
2017-03-31 19:39:37 -0700

    fixed merge issues

--- server/server.js
+++ server/server.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const axios = require('axios')
 const db = require('../db/startdb.js')
-const router = require('./router.js')
+const bindrouter = require('./router.js')
 
 const util = require('./util/util.js')
-=======
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const axios = require('axios');
-// const db = require('../db/startdb.js');
-
-const PORT = process.env.PORT || 8000;
->>>>>>> master
 
 
 const PORT = process.env.PORT || 8000
@@ -25,13 +15,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public')))
 
-
-router(app)
-// app.route('/hello')
-//   .get(function(req, res){
-//     res.send('okayy')
-//   })
-
+bindrouter(app)
 
 app.listen(PORT, function() {
   console.log('Obento express server connection established at:', PORT);
@@ -39,10 +23,3 @@ app.listen(PORT, function() {
 
 exports.app = app 
 
-// app.route('/api/noris')
-//   .post(util.tasks.noris.post)
-//   .get(util.tasks.noris.get)
-
-// app.route('/api/bentos')
-//   .post(util.tasks.bentos.post)
-//   .get(util.tasks.bentos.get)
