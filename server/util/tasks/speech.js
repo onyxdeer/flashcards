b82f56c321fbd6f73c7479af50e7b99f70acec6a
Patrick Xie <Patrick.y.xie@gmail.com>
2017-04-04 16:46:07 -0700

    speech recognition is routed thru google

--- server/util/tasks/speech.js
+++ server/util/tasks/speech.js
@@ -1,33 +1,47 @@
-const fs = require('fs');
-
-// Imports the Google Cloud client library
+const record = require('node-record-lpcm16');
 const Speech = require('@google-cloud/speech');
-
-// Instantiates a client
 const speech = Speech();
 
-// The path to the local file on which to perform speech recognition, e.g. /path/to/audio.raw
-// const filename = '/path/to/audio.raw';
-
 // The encoding of the audio file, e.g. 'LINEAR16'
-// const encoding = 'LINEAR16';
+const encoding = 'LINEAR16';
 
 // The sample rate of the audio file, e.g. 16000
-// const sampleRate = 16000;
+const sampleRate = 16000;
 
-const request = {
-  config: {
-    encoding: encoding,
-    sampleRate: sampleRate
-  }
-};
 
-// Stream the audio to the Google Cloud Speech API
-const recognizeStream = speech.createRecognizeStream(request)
-  .on('error', console.error)
-  .on('data', (data) => {
-    console.log('Data received: %j', data);
-  });
 
-// Stream an audio file from disk to the Speech API, e.g. "./resources/audio.raw"
-fs.createReadStream(filename).pipe(recognizeStream);
+
+const get = (req, res) => {
+  console.log('Listening, press Ctrl+C to stop.');
+
+  const request = {
+    config: {
+      encoding: encoding,
+      sampleRate: sampleRate
+    }
+  };
+
+  console.log('request no problem')
+  // Create a recognize stream
+  const recognizeStream = speech.createRecognizeStream(request)
+    .on('error', function(){
+      console.log('we have an error in stream');
+      console.error;
+    })
+    .on('data', (data) => process.stdout.write(data.results));
+
+  // Start recording and send the microphone input to the Speech API
+  record.start({
+    sampleRate: sampleRate,
+    threshold: 0
+  }).pipe(recognizeStream);
+
+}
+
+const post = (req, res) => {
+  console.log('Listening, press Ctrl+C to stop.');
+
+}
+
+
+module.exports = { get, post };
