7c452a4c1da5fbdda24673185d1726d0353d21a4
David Zou <david.zou@outlook.com>
2017-04-04 11:29:19 -0700

    Added Voice page for testing

---
+++ client/components/Voice/Voice.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Voice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Hello World from Voice!</div>
+    )
+  }
+}
+
+export default Voice;
