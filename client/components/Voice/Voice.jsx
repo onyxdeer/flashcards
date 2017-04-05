a1bb4f259054523befec59c501e9256585ded203
Patrick Xie <Patrick.y.xie@gmail.com>
2017-04-04 19:23:34 -0700

    added voice rec on front end

--- client/components/Voice/Voice.jsx
+++ client/components/Voice/Voice.jsx
@@ -7,6 +7,26 @@ class Voice extends Component {
     this.state = {
 
     }
+
+    if (annyang) {
+    // Let's define our first command. First the text we expect, and then the function it should call
+    var commands = {
+      'start': function() {
+        // $('#tpsreport').animate({bottom: '-100px'});
+        console.log('hello')
+      },
+      'hello': function(){
+        console.log('just work baby')
+      }
+    };
+
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
+  }
+
   }
 
   render() {
