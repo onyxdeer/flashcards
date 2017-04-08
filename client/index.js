42712a04c5a3d4672ee50d3a7b8f5bd123945fbe
Patrick Xie <Patrick.y.xie@gmail.com>
2017-04-08 16:00:43 -0700

    imported redux libraries

--- client/index.js
+++ client/index.js
@@ -1,8 +1,14 @@
 'use strict';
+// import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers/reducers';
+import thunk from 'redux-thunk';
 
 ReactDOM.render( <App />, document.getElementById('app'));
 
---
+++ client/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+// import { routeReducer } from 'react-router-redux';
+
+import test from './testreducer'
+
+const reducers = combineReducers({
+  // routing: routeReducer
+  // test
+});
+
+export default reducers;
