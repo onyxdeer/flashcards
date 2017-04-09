10754aab7b6ed6a6cc410b2f9d4eef085f132886
Patrick Xie <Patrick.y.xie@gmail.com>
2017-04-08 18:48:10 -0700

    finished seting up react redux component and action

--- client/actions/actionTypes.js
+++ client/actions/actionTypes.js
@@ -2,6 +2,9 @@
 // this also acts as a hub in case we need to change the action type names
 // make new action types and put it in here and export them
 
+//also you want to make sure you are using these CONST variables instead of just strings when you specify action types
+//because if you misspell a string it'll be impossible to find out
+//but if you misspell a variable it'll throw an error which you can then debug
 export const GET_HELLO = 'GET_HELLO';
 export const POST_HELLO = 'POST_HELLO';
 export const WAVE_HELLO = 'WAVE_HELLO'
--- client/components/SampleReduxComponent.jsx
+++ client/components/SampleReduxComponent.jsx
@@ -1,4 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+// connect is what we use to bind the component to redux store
+import { connect } from 'react-redux';
+
+import * as actions from './actions/sampleActions.js'
 
 class SampleReduxComponent extends Component {
   constructor(props) {
@@ -9,13 +13,30 @@ class SampleReduxComponent extends Component {
   render() {
     return (
       <div>
-          {this.props.hello}
+          {this.props.componentPropData}
       </div>
     );
   }
 }
 
 
-export default SampleReduxComponent;
+
+
+
+//we map the state to props where the keys are the props on this component and the values are the corresponding state tree from the store
+function mapStateToProps(state) {
+  return { 
+    //these are just sample names
+    // userData: state.recipes.userRecipes,
+    // viewFollows: state.follows.dataForUser,
+    // favorites: state.favorites.dataForUser,
+    // data: state.recipes.data
+    componentPropData: state.data
+  }
+}
+
+export default connect(mapStateToProps, actions)(SampleReduxComponent);
+
+// export default SampleReduxComponent;
 
 
