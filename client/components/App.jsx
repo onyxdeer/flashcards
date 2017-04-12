a425e501092dbdd218eb2b5510d250e8b77aa232
Eric Chen <saungchi@gmail.com>
2017-04-11 21:30:31 -0700

    Refactor App component into redux

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -15,6 +15,9 @@ import Search from './Search/Search.jsx';
 import User from './User/User.jsx';
 import Voice from './Voice/Voice.jsx';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions/appActions.jsx';
+
 // Allows redirection from current page to Search page upon search submission
 let TargetRoute = () => ( <Redirect from='/' to='/' /> );
 
@@ -22,59 +25,60 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      query: '',
-      searchActive: false,
-      userId: 'guest',
-      bentoId: null
-    }
+    // this.state = {
+    //   query: '',
+    //   searchActive: false,
+    //   userId: 'guest',
+    //   bentoId: null
+    // }
 
-    this.handleNavSubmit = this.handleNavSubmit.bind(this);
-    this.endNavSubmit = this.endNavSubmit.bind(this);
-    this.setBentoId = this.setBentoId.bind(this);
-    this.setUserId = this.setUserId.bind(this);
+    // this.handleNavSubmit = this.handleNavSubmit.bind(this);
+    // this.endNavSubmit = this.endNavSubmit.bind(this);
+    // this.setBentoId = this.setBentoId.bind(this);
+    // this.setUserId = this.setUserId.bind(this);
   }
 
   // gets called when user pushes the submit button or presses enter
-  handleNavSubmit(event, input) {
-    console.log('calling handleNavSubmit with event:', event)
-    console.log('calling handleNavSubmit with input:', input);
-    event.preventDefault();
-    var context = this;
-    this.setState({
-      query: input,
-      searchActive: true
-    }, () => { 
-      console.log('searchActive in handleNavSubmit:', context.state.searchActive);
-      console.log('query changed to:', context.state.query);
-     });
-  }
+  // handleNavSubmit(event, input) {
+  //   console.log('calling handleNavSubmit with event:', event)
+  //   console.log('calling handleNavSubmit with input:', input);
+  //   event.preventDefault();
+  //   var context = this;
+  //   this.setState({
+  //     query: input,
+  //     searchActive: true
+  //   }, () => { 
+  //     console.log('searchActive in handleNavSubmit:', context.state.searchActive);
+  //     console.log('query changed to:', context.state.query);
+  //    });
+  // }
 
-  // ends the submit action
-  endNavSubmit() {
-    if (this.state.searchActive === true) {
-      this.setState({
-        searchActive: false
-      });
-    }
-  }
+  // // ends the submit action
+  // endNavSubmit() {
+  //   if (this.state.searchActive === true) {
+  //     this.setState({
+  //       searchActive: false
+  //     });
+  //   }
+  // }
 
-  setBentoId(id) {
-    this.setState({
-      bentoId: id
-    });
-  }
+  // setBentoId(id) {
+  //   this.setState({
+  //     bentoId: id
+  //   });
+  // }
 
-  setUserId(id) {
-    this.setState({
-      userId: id
-    });
-  }
+  // setUserId(id) {
+  //   this.setState({
+  //     userId: id
+  //   });
+  // }
 
   render() {
 
     // triggers a redirection to Search page if 'searchActive' state is triggered from submission action
-    if (this.state.searchActive) {
+    if (this.props.searchActive) {
+      console.log('YOYOYO');
       TargetRoute = () => ( <Redirect to='/search' /> )
     }
 
@@ -82,14 +86,14 @@ class App extends Component {
       <div>
         <Router>
           <div>
-            <Nav handleNavSubmit={this.handleNavSubmit} userId={this.state.userId} setBentoId = {this.setBentoId}/>
+            <Nav handleNavSubmit={this.handleNavSubmit} userId={this.props.userId} setBentoId = {this.setBentoId}/>
             <TargetRoute />
             <Route exact path='/' component={() => <Landing />} />
-            <Route path='/display' component={() => <Display bentoId={this.state.bentoId}/>} />
+            <Route path='/display' component={() => <Display bentoId={this.props.bentoId}/>} />
             <Route path='/landing' component={() => <Landing />} />
-            <Route path='/edit' component={() => <Edit userId = {this.state.userId} bentoId = {this.state.bentoId} setBentoId = {this.setBentoId}/>}/>
-            <Route path='/search' component={() => <Search query={this.state.query} endNavSubmit={this.endNavSubmit} userId = {this.state.userId}  bentoId = {this.state.bentoId} setBentoId = {this.setBentoId}/>} />
-            <Route path='/user' component={() => <User userId = {this.state.userId} bentoId = {this.state.bentoId} setBentoId = {this.setBentoId}/> } />
+            <Route path='/edit' component={() => <Edit userId = {this.props.userId} bentoId = {this.props.bentoId} setBentoId = {this.setBentoId}/>}/>
+            <Route path='/search' component={() => <Search query={this.props.query} endNavSubmit={this.endNavSubmit} userId = {this.props.userId}  bentoId = {this.props.bentoId} setBentoId = {this.setBentoId}/>} />
+            <Route path='/user' component={() => <User userId = {this.props.userId} bentoId = {this.props.bentoId} setBentoId = {this.setBentoId}/> } />
             <Route path='/voice' component={() => <Voice />} />
           </div>
         </Router>
@@ -98,4 +102,13 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return { 
+    query: state.appReducer.query,
+    userId: state.appReducer.userId,
+    bentoId: state.appReducer.bentoId,
+    searchActive: state.appReducer.searchActive
+  }
+}
+
+export default connect(mapStateToProps, actions)(App);
