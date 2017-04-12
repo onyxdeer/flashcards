0c6eb9be09f4f4ba93269a23a179110b3e1ab20a
Eric Chen <saungchi@gmail.com>
2017-04-11 21:29:10 -0700

    Fixed bugs

--- client/actions/appActions.jsx
+++ client/actions/appActions.jsx
@@ -1,4 +1,42 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 
-import { UPDATE_QUERY, GET_USER_ID, GET_BENTO_ID, CHANGE_SEARCH_ACTIVE } from './actionTypes.js'
+import { UPDATE_QUERY, GET_USERID, GET_BENTOID, CHANGE_SEARCHACTIVE } from './actionTypes.js'
+
+export function handleNavSubmit(event, input) {
+  event.preventDefault();
+  return function(dispatch, getState) {
+    dispatch({
+      type: UPDATE_QUERY,
+      query: input,
+      searchActive: true
+    });
+  }    
+}
+
+export function handleNavSearch(event) {
+  return function(dispatch, getState) {
+    dispatch({ type: CHANGE_INPUT, input: event.target.value });
+  }
+}
+
+// ends the submit action
+export function endNavSubmit() {
+  return function(dispatch, getState) {
+    if (searchActive === true) {      
+      dispatch({ type: CHANGE_SEARCHACTIVE, searchActive: false });
+    }
+  }
+}
+
+export function setBentoId(id) {
+  return function(dispatch, getState) {
+    dispatch({ type: GET_BENTOID, bentoId: id });
+  }
+}
+
+export function setUserId(id) {
+  return function(dispatch, getState) {
+    dispatch({ type: GET_USERID, userId: id });
+  }  
+}
