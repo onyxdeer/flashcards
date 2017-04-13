a163c9679ccf8fc9e04a78301e7012b75bb300e2
Patrick Xie <Patrick.y.xie@gmail.com>
2017-04-13 15:57:44 -0700

    re-enabling voice components

--- client/components/Voice/Voice.jsx
+++ client/components/Voice/Voice.jsx
@@ -1,20 +1,36 @@
-// import React, { Component } from 'react';
-// import axios from 'axios';
-// import { commands, noris, responses } from './util.js';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { commands, noris, responses } from './util.js';
 
-// class Voice extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
+class Voice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-//     }
+    }
 
-//     if (annyang) {
-//       annyang.addCommands(commands);
-//       annyang.start();
-//     }
+    if (annyang) {
+      annyang.addCommands(commands);
+      annyang.start();
+    }
 
-//     var session = {
+
+    
+
+  }
+
+  render() {
+    return (
+      <div>Hello World from Voice!</div>
+    )
+  }
+}
+
+export default Voice;
+
+
+
+//   var session = {
 //       audio: true,
 //       video: false
 //     };
@@ -58,18 +74,3 @@
 //   console.log('weve opened a client')
 //   window.Stream = client.createStream();
 // }
-
-
-    
-
-//   }
-
-//   render() {
-//     return (
-//       <div>Hello World from Voice!</div>
-//     )
-//   }
-// }
-
-// export default Voice;
-
