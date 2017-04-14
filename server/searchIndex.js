a77f67eef23e5b8d0061e2af1819b73b190bf770
Eric Chen <saungchi@gmail.com>
2017-04-14 16:35:14 -0700

    Send bento data to AWS ES cluster

--- server/searchIndex.js
+++ server/searchIndex.js
@@ -1,63 +1,40 @@
 const $search = require('./elasticSearch');
-const _ = require('lodash');
-// const Promise = require('bluebird');
-// const db = require('../db/db')
-// const query = Promise.promisify(db.query.bind(db));
+const Sequelize = require('sequelize');
+const Promise = require('bluebird'); // Save to dependency
 
-function returnRecipe(id) {
-  const ingredientsQuery = `SELECT * from ingredients WHERE recipe_id = ${id};`;
-  const tagsQuery = `SELECT * from tags WHERE recipe_id = ${id};`;
-  const recipeQuery = `SELECT * from recipes WHERE id = ${id};`
-  const userQuery = `SELECT username FROM users WHERE id = (SELECT user_id FROM recipes WHERE id = ${id})`
+const Bento = require('../db/models/bentos.js');
 
-  query(recipeQuery).then(([recipe]) => {
-    if(!recipe) return res.status(422).send({ error: 'Recipe does not exist' });
-  })
+function returnBento(id) {
+  return Bento.findOne({where: {id: id}})
+    .then((bento) => {
+      const { id, name, id_hash, description, nori_count, private, visit_count, user_id, category_id } = bento;
+      return {
+        id,
+        name,
+        id_hash,
+        description,
+        nori_count,
+        private,
+        visit_count,
+        user_id,
+        category_id        
+      }
+    })
+    .catch((err) => console.log(err));
+};
 
-  return Promise.all([query(ingredientsQuery), query(tagsQuery), query(recipeQuery), query(userQuery)])
-    .then(([ ingredients, tagList, [ recipe ], [ user ] ]) => {
-    const [ quantity, items ] = ingredients.reduce((acc, { quantity, ingredient }) => {
-      return [ [...acc[0], quantity], [...acc[1], ingredient] ];
-    }, [[], []])
-    const tags = tagList.reduce((arr, obj) => {
-      return [...arr, obj.tag_name]
-    },[])
-    const { id, name, image, difficulty, cook_time, prep_time, servings, instructions, user_id, description, parent_id } = recipe;
-    const { username } = user;
-    return {
-      id,
-      parent_id,
-      image,
-      username, 
-      name, 
-      difficulty, 
-      description,
-      cook_time, 
-      prep_time, 
-      servings, 
-      instructions, 
-      ingredients: { quantity, items },
-      user_id,
-      tags
-    }
-  })
-}
-const getAllRecipesQuery = `SELECT * from recipes;`
-
-query(getAllRecipesQuery)
-  .then(recipes => Promise.all(recipes.map(recipe => returnRecipe(recipe.id))))
-  .then(details => {
-    details.map((item, i) => {
+Bento.findAll()
+  .then(bentos => {
+    console.log(bentos.data);
+    return Promise.all(bentos.data.map(bento => returnBento(bento.id)));
+   })
+  .then(bentosES => {
+    bentosES.map((bentoES, i) => {
       $search.index({
-        index: 'recipes',
+        index: 'bentos',
         type: 'Object',
         id: i,
-        body: item
+        body: bentoES
       }, (error, response) => {});
     })
   })
-
-
-// $search.indices.delete({
-//   index: 'recipes'
-// })
