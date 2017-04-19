4d485d3c7f7fe9fc63623c8b65c05000e40838b2
David Zou <david.zou@outlook.com>
2017-04-18 18:08:18 -0700

    [wip] add input response to login and register pages.

--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { checkLogin, handleEmailInput, handlePasswordInput, clearAuthInputs } from '../../actions/authActions.js';
 
 class Login extends Component {
   constructor(props) {
     super(props);
 
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(event) {
+    event.preventDefault();
+    this.props.checkLogin(this.props.emailAddress, this.props.password);
+  }
+
+  componentWillUnmount() {
+    this.props.clearAuthInputs();
   }
 
   render() {
     return (
       <div className="container">
         <h1>Log In</h1>
-        <form action="/authenticate" method="post">
+        <form onSubmit={this.handleLogin}>
           <div className="form-group row">
             <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
             <div className="col-sm-10">
-              <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
+              <input type="email" className="form-control" id="inputEmail" placeholder="Email" value={this.props.emailAddress} onChange={(event) => this.props.handleEmailInput(event)} />
             </div>
           </div>
           <div className="form-group row">
             <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
             <div className="col-sm-10">
-              <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+              <input type="password" className="form-control" id="inputPassword" placeholder="Password" value={this.props.password} onChange={(event) => this.props.handlePasswordInput(event)} />
             </div>
           </div>
           <div className="form-group row">
@@ -45,7 +56,9 @@ class Login extends Component {
 
 function mapStateToProps(state) {
   return {
+    emailAddress: state.authReducer.emailAddress,
+    password: state.authReducer.password,
   };
 }
 
-export default connect(mapStateToProps)(Login);
+export default connect(mapStateToProps, { checkLogin, handleEmailInput, handlePasswordInput, clearAuthInputs })(Login);
