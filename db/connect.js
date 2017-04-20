ebc7cd6d02c447f3aa3c2041bc125a08dd6efa40
Eric Chen <saungchi@gmail.com>
2017-04-20 11:13:38 -0700

    [doc] Clean up code

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,6 @@
 const Sequelize = require('sequelize');
 
 // Set up database connection
-const db = new Sequelize('obentoDB', 'root', '', {
-  dialect: 'mysql', // or 'sqlite', 'postgres', 'mariadb'
-});
+const db = new Sequelize('obentoDB', 'root', '');
 
 module.exports = db;
