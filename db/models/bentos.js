d05517dd460ca8a555115c7ad789ccee5820b7d4
Eric Chen <saungchi@gmail.com>
2017-04-20 12:23:54 -0700

    [doc] Clean up code

--- db/models/bentos.js
+++ db/models/bentos.js
@@ -1,40 +1,34 @@
 const Sequelize = require('sequelize');
 const db = require('../connect.js');
 const User = require('./users.js');
-const Category = require('./categories.js');
 
 const Bento = db.define('bento', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
   id_hash: Sequelize.STRING,
   description: Sequelize.STRING,
   nori_count: Sequelize.INTEGER,
   private: {
     type: Sequelize.BOOLEAN,
-    defaultValue: false
+    defaultValue: false,
   },
   visit_count: Sequelize.INTEGER,
   user_id: {
     type: Sequelize.INTEGER,
     references: {
       model: User,
-      key: 'id'
-    }
+      key: 'id',
+    },
   },
   category_id: {
     type: Sequelize.INTEGER,
     references: {
       model: 'categories',
-      key: 'id'
-    }
-  }
+      key: 'id',
+    },
+  },
 });
 
 module.exports = Bento;
