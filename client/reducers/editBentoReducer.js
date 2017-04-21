ca34b0debbbdeb9aa321ab378726f465d765763b
Eric Chen <saungchi@gmail.com>
2017-04-21 11:35:21 -0700

    [fix] Refactor redux reducer files to be compliant with Airbnb linter

--- client/reducers/editBentoReducer.js
+++ client/reducers/editBentoReducer.js
@@ -1,108 +1,88 @@
-import {HANDLE_EDIT_BENTO_INFO, HANDLE_SAVE_BENTO, HANDLE_NORI_CHANGE, HANDLE_ADD_NEW_NORI, HANDLE_DELETE_NORI, HANDLE_FETCH_BENTO_FOR_EDIT, HANDLE_RENDER_CREATE_PAGE,
-HANDLE_IMAGE_UPLOAD} from '../actions/actionTypes.js'
 import RichTextEditor from 'react-rte';
-import {convertToRaw} from 'draft-js';
-//This is the default state y
+import { convertToRaw } from 'draft-js';
+import { HANDLE_EDIT_BENTO_INFO, HANDLE_SAVE_BENTO, HANDLE_NORI_CHANGE, HANDLE_ADD_NEW_NORI, HANDLE_DELETE_NORI, HANDLE_FETCH_BENTO_FOR_EDIT, HANDLE_RENDER_CREATE_PAGE, HANDLE_IMAGE_UPLOAD } from '../actions/actionTypes';
+
+// This is the default state y
 const empty = JSON.stringify(convertToRaw(RichTextEditor.createEmptyValue()._editorState.getCurrentContent()));
-const DEFAULT_STATE =  {
+
+const stateDefault = {
   name: '',
   description: '',
   category: '',
   visit_count: 0,
   bento_id: null,
   user_id: 1 || 'guest',
+  noris: [{ Front: { image: null, text: empty, soundFile: null }, Back: { image: null, text: empty, soundFile: null } }, { Front: { image: null, text: empty, soundFile: null }, Back: { image: null, text: empty, soundFile: null } }],
+};
 
-  noris: [{Front: {image: null, text: empty, soundFile: null}, Back: {image: null, text:empty, soundFile: null}}, {Front: {image: null, text:empty, soundFile: null}, Back: {image: null, text:empty, soundFile: null}}]
-
-}
-
-//this handler resets the create page back to default whenever a user clicks create
-
+// This handler resets the create page back to default whenever a user clicks create
 function handleRenderCreatePage(state, action) {
-  var def = {
-  name: '',
-  description: '',
-  category: '',
-  visit_count: 0,
-  bento_id: null,
-  user_id: 1 || 'guest',
-  noris: [{Front: {image: null, text:empty, soundFile: null}, Back: {image: null, text:empty, soundFile: null}},{Front: {image: null, text:empty, soundFile: null}, Back: {image: null, text:empty, soundFile: null}}]
-}
-  return {...state, ...def}
-}
-
-
-//---------------------------------------------------------------
-
-
-function handleImageUpload(state, action) {
-  return{...state, noris: action.payload}
+  const def = {
+    name: '',
+    description: '',
+    category: '',
+    visit_count: 0,
+    bento_id: null,
+    user_id: 1 || 'guest',
+    noris: [{ Front: { image: null, text: empty, soundFile: null }, Back: { image: null, text: empty, soundFile: null } }, { Front: { image: null, text: empty, soundFile: null }, Back: { image: null, text: empty, soundFile: null } }],
+  };
+  return { ...state, ...def };
 }
 
+const handleImageUpload = (state, action) => ({
+  ...state,
+  noris: action.payload,
+});
 
-//---------------------------------------------------------------
+const handleFetchBentoForEdit = (state, action) => ({
+  ...state,
+  ...action.payload,
+});
 
+const handleNoriChange = (state, action) => ({
+  ...state,
+  noris: action.payload,
+});
 
-function handleFetchBentoForEdit(state, action) {
-  console.log('helloo?? line 16 of editBentoReducer', action.payload)
-  return {...state, ...action.payload}
-}
+const handleEditBentoInfo = (state, action) => {
+  const val = action.payload.target.value;
+  return { ...state, [action.payload.target.name]: val };
+};
 
-//-------------------------------------------------------------
+const handleSaveBento = (state, action) => ({
+  ...state,
+  bento_id: action.payload,
+});
 
-function handleUpdateRTEState(state, action) {
-  return {...state, value: action.payload}
-} 
+const handleAddNewNori = (state, action) => ({
+  ...state,
+  noris: action.payload,
+});
 
-function handleNoriChange(state, action) {
-  return {...state, noris: action.payload}
-}
-//-------------------------------------------------------------
+const handleDeleteNori = (state, action) => ({
+  ...state,
+  noris: action.payload,
+});
 
-function handleEditBentoInfo(state, action) {
-  let val = action.payload.target.value
-  return {...state, [action.payload.target.name] : val}
-}
-
-function handleSaveBento (state, action) {
-  return {...state, bento_id: action.payload}
-}
-
-function handleAddNewNori (state, action) {
-  return {...state, noris: action.payload}
-}
-
-function handleDeleteNori (state, action) {
-  return {...state, noris: action.payload}
-}
-
-export default function(state = DEFAULT_STATE, action) {
-  switch(action.type) {
+export default (state = stateDefault, action) => {
+  switch (action.type) {
     case HANDLE_EDIT_BENTO_INFO:
-    return handleEditBentoInfo(state, action)
-
+      return handleEditBentoInfo(state, action);
     case HANDLE_SAVE_BENTO:
-    return handleSaveBento(state, action)
-
+      return handleSaveBento(state, action);
     case HANDLE_ADD_NEW_NORI:
-    return handleAddNewNori(state, action)
-
+      return handleAddNewNori(state, action);
     case HANDLE_DELETE_NORI:
-    return handleDeleteNori(state,action)
-
+      return handleDeleteNori(state, action);
     case HANDLE_NORI_CHANGE:
-    return handleNoriChange(state, action)
-
+      return handleNoriChange(state, action);
     case HANDLE_FETCH_BENTO_FOR_EDIT:
-    return handleFetchBentoForEdit(state, action)
-
+      return handleFetchBentoForEdit(state, action);
     case HANDLE_RENDER_CREATE_PAGE:
-    return handleRenderCreatePage(state, action)
-
+      return handleRenderCreatePage(state, action);
     case HANDLE_IMAGE_UPLOAD:
-    return handleImageUpload(state, action)
+      return handleImageUpload(state, action);
+    default:
+      return state;
   }
-
-
-  return state
-}
+};
