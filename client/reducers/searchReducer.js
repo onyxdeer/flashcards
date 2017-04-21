7bfb61e9e90b22f6a264a49770f130f76e16c308
David Zou <david.zou@outlook.com>
2017-04-21 14:32:59 -0700

    [fix] fix css issue for headers, navbar overlapped page titles

--- client/reducers/searchReducer.js
+++ client/reducers/searchReducer.js
@@ -1,8 +1,13 @@
 import { FIND_BENTOS } from '../actions/actionTypes';
 
-const stateDefault = [];
+const stateDefault = {
+  bentos: null,
+};
 
-const handleSearchBentos = (state, action) => action.payload;
+const handleSearchBentos = (state, action) => ({
+  ...state,
+  bentos: action.payload
+});
 
 export default (state = stateDefault, action) => {
   switch (action.type) {
