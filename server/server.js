f443675de915cb029cffd534b545136e8134998b
Patrick Xie <Patrick.y.xie@gmail.com>
2017-04-23 13:50:40 -0700

    [fix] resolve server conflict

--- server/server.js
+++ server/server.js
@@ -4,18 +4,14 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const path = require('path');
 const morgan = require('morgan');
-<<<<<<< HEAD
-var history = require('connect-history-api-fallback');
 const Http = require('http')
 const Sockets = require('socket.io')
-=======
 const history = require('connect-history-api-fallback');
 const passport = require('passport');
 const flash = require('connect-flash');
 const database = require('../db/setup.js');
 const bindrouter = require('./router.js');
 const { SESSION_SECRET } = require('../config/config.js');
->>>>>>> feature-main-bugfix
 
 const PORT = process.env.PORT || 8000;
 
@@ -39,7 +35,6 @@ bindrouter(app);
 
 database();
 
-<<<<<<< HEAD
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
@@ -59,12 +54,10 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(PORT, function() {
+http.listen(PORT, () => {
   console.log('Obento express server connection established at:', PORT);
 });
-=======
-app.listen(PORT, () => console.log('Obento express server connection established at:', PORT));
->>>>>>> feature-main-bugfix
+// app.listen(PORT, () => console.log('Obento express server connection established at:', PORT));
 
 exports.app = http;
 // exports.socket = socket
