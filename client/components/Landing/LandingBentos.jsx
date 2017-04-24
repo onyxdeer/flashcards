37d259e7b4ef4132b24bb17192ae9c417836f308
wichew <wiltonchew@gmail.com>
2017-04-24 02:40:41 -0700

    [feat]animates more animations options on landing page

--- client/components/Landing/LandingBentos.jsx
+++ client/components/Landing/LandingBentos.jsx
@@ -3,10 +3,39 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux'
 import * as actions from '../../actions/appActions.js'
 
-    var wowAnimation = ["wow landing-bento animated bounceIn", "wow landing-bento animated bounceIn", "wow landing-bento animated bounceInLeft",
-    "wow landing-bento animated bounceInRight","wow landing-bento animated bounceInUp","wow landing-bento animated bounce","wow landing-bento animated shake",
-    "wow landing-bento animated tada","wow landing-bento animated rubberBand","wow landing-bento animated wobble","wow landing-bento animated jello",
-    "wow landing-bento animated flash","wow landing-bento animated slideInUp","wow landing-bento animated rollIn","wow landing-bento animated pulse"]
+    var wowAnimation = [
+    "wow landing-bento animated bounce",
+    "wow landing-bento animated flash",
+    "wow landing-bento animated pulse", 
+    "wow landing-bento animated rubberBand", 
+    "wow landing-bento animated shake",
+    "wow landing-bento animated headShake",
+    "wow landing-bento animated swing",
+    "wow landing-bento animated tada",
+    "wow landing-bento animated wobble",
+    "wow landing-bento animated jello",
+    "wow landing-bento animated bounceIn", 
+    "wow landing-bento animated bounceInUp", 
+    "wow landing-bento animated bounceInLeft",
+    "wow landing-bento animated bounceInRight",
+    "wow landing-bento animated fadeIn",
+    "wow landing-bento animated fadeInLeft",
+    "wow landing-bento animated fadeInLeftBig",
+    "wow landing-bento animated flipInX",
+    "wow landing-bento animated flipInY",
+    "wow landing-bento animated lightSpeedIn",
+    "wow landing-bento animated rotateIn", 
+    "wow landing-bento animated rotateInUpLeft", 
+    "wow landing-bento animated rotateInUpRight",
+    "wow landing-bento animated rollIn",
+    "wow landing-bento animated zoomIn",
+    "wow landing-bento animated zoomInLeft",
+    "wow landing-bento animated zoomInRight",
+    "wow landing-bento animated zoomInUp",
+    "wow landing-bento animated slideInLeft",
+    "wow landing-bento animated slideInRight",
+    "wow landing-bento animated slideInUp"
+    ]
 
 class LandingBentos extends Component {
   constructor(props) {
@@ -16,7 +45,7 @@ class LandingBentos extends Component {
   render() {
     console.log(this.props.landing)
     var bento = this.props.bento
-    var animationNumber = Math.floor(Math.random()*15);
+    var animationNumber = Math.floor(Math.random()*31);
     return (
 <div className = {wowAnimation[animationNumber]} data-wow-delay = "0.8s" data-wow-duration="2.5s" style = {{display: 'flex', flexDirection: 'column', justifyContent:'space-between', flexGrow: 0, flexShrink: 0}}>
     <div className="view overlay hm-white-slight">
