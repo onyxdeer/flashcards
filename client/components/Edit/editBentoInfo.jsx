06d7c4750ac539fe8ff3020870f2a15943d8463c
wichew <wiltonchew@gmail.com>
2017-05-01 14:49:50 -0700

    [bug]Label for bentoname is set to default active on edit page render

--- client/components/Edit/editBentoInfo.jsx
+++ client/components/Edit/editBentoInfo.jsx
@@ -55,8 +55,8 @@ class editBentoInfo extends React.Component {
                 <div className="md-form">
                     {/*<form id="contact-us">*/}
                         <div className="col-xs-6 wow animated slideInLeft" data-wow-delay = ".4s">
-                            <input type="text" name="name" id="form1" required="required" value={this.props.bento.name} className="form-control" onChange = {(event) => {this.props.handleChange(event)}}/>
-                            <label className="">Bento Name</label>
+                            <input type="text" name="name" id="form1" required="required" value={this.props.bento.name} className="form-control focusedInput" onChange = {(event) => {this.props.handleChange(event)}}/>
+                            <label className="active">Bento Name</label>
                             {/*<input type="text" name="category" id="subject" required="required" value={this.props.bento.category} className="form-control" placeholder="Subject" onChange = {(event) => {this.props.handleChange(event)}}/>*/}
                             {/*<label for="form1" class="">Category</label>*/}
                               {this.props.bento.cover.url ? <img src = {this.props.bento.cover.url} className = "img-fluid"/> : <div className = {'dropzone' + ' dzcover col-xs-6 pull-right'}></div>}
