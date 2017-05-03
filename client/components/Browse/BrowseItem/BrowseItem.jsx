399501e1d07f270dbf4ad7d5ab24572016848e98
Eric Chen <saungchi@gmail.com>
2017-05-02 20:54:12 -0700

    [feature] Keep button look consistent with rest of app

--- client/components/Browse/BrowseItem/BrowseItem.jsx
+++ client/components/Browse/BrowseItem/BrowseItem.jsx
@@ -29,8 +29,7 @@ class BrowseItem extends Component {
             <h3 className='browse-title'>{this.props.item.name}</h3>
             <p className='bentoDescription exploreItems'>{this.props.item.description}</p>
             <p className='exploreItems'><label>View Count:</label> {this.props.item.visit_count} </p>
-            <p className='exploreItems'><Link className='btn btn-primary' to={'/display'} onClick={() => this.props.setBentoId(this.props.item.id)}><i className="fa fa-eye" aria-hidden="true"></i></Link><span>   </span><Link className='btn btn-default' to={'/edit'} onClick={() => this.props.handleFetchBentoForEdit(this.props.bento, this.props.item.id, this.props.userId)}><i className="fa fa-pencil" aria-hidden="true"></i></Link></p>
-            <button className="speechButton btn btn-success" onClick={this.modalAndFetchNori.bind(this, this.props.item.id)}><i className="fa fa-volume-up" aria-hidden="true"></i></button>
+            <p className='exploreItems'><Link className='btn btn-primary btn-sm' to={'/display'} onClick={() => this.props.setBentoId(this.props.item.id)}><i className="fa fa-eye" aria-hidden="true"></i></Link><button className="speechButton btn btn-success btn-sm" onClick={this.modalAndFetchNori.bind(this, this.props.item.id)}><i className="fa fa-volume-up" aria-hidden="true"></i></button><Link className='btn btn-default btn-sm' to={'/edit'} onClick={() => this.props.handleFetchBentoForEdit(this.props.bento, this.props.item.id, this.props.userId)}><i className="fa fa-pencil" aria-hidden="true"></i></Link></p>
           </div>
         </span>
       </div>
