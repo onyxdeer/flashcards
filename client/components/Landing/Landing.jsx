81d68150f00e69f1cd639ee5c12b0987dc59453a
Eric Chen <saungchi@gmail.com>
2017-05-06 16:33:22 -0700

    [feature] Add more descriptive Obento blurb on landing page

--- client/components/Landing/Landing.jsx
+++ client/components/Landing/Landing.jsx
@@ -56,7 +56,7 @@ class Landing extends Component {
                           <h1 className="lp wow animated fadeInDown">Welcome to Obento!</h1>
                           <hr style = {{marginTop: 10}} className= "wow animated slideInLeft"/>
 	                        <h2 className="lp wow fadeInDown">Memorization made fun and effortless.</h2>
-	                        <p className="lp wow fadeInDown" data-wow-delay="0.3s">Get started by creating a bento of noris, then interact seamlessly with your noris by using hands-free voice control!</p>
+	                        <p className="lp wow fadeInDown" data-wow-delay="0.3s">Build a new bento (deck) of noris (flashcards) or find one created by others. Then interact seamlessly with your noris by using hands-free voice control!</p>
 	                        <div className="wow fadeInDown" data-wow-delay="0.3s">
 	                        	<Link to="/edit" className="btn btn-default btn-home" role="button" onClick={()=> {this.props.handleRenderCreatePage()}}>Create Your Own Bento!</Link>
 	                        </div>
